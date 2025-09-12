7f4c89de39c0054f9dfcad108c1ebb8015f5e5c0
ltn <ltn.sunrise@gmail.com>
2025-09-12 17:12:43 +0700

    refactor: remove fe

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,10 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'prettier'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   // using the latest ECMAScript version and ESM
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  //  react plugin to detect our installed React version automatically
-  settings: { react: { version: 'detect' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
@@ -20,10 +12,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-  // overrides array: specify that ESLint should only lint .js and .jsx files
-  overrides: [
-    {
-      files: ['*.js', '*.jsx'],
-    },
-  ],
 }
